671422c84748c91301a2628db7ea4e3dbe91802e
student-wroclaw <kingakusa@gmail.com>
2025-05-17 14:24:57 +0200

    3.9

---
+++ FleetMind/Fleet_/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path
+from .views import hello_users, hello_name, table, post_list, create_post, update_post, delete_post, chat, register, \
+    user_panel, CustomLoginView
+from django.contrib.auth.views import LogoutView, LoginView
+
+urlpatterns = [
+    path('hello/', hello_users),
+    path('hello/<str:name>/', hello_name),
+    path('table/', table, name='table'),
+    path('posts/', post_list, name='post_list'),
+    path('posts/create/', create_post, name='create_post'),
+    path('posts/update/<int:post_id>', update_post, name='update_post'),
+    path('posts/delete/<int:post_id>', delete_post, name='delete_post'),
+    path('chat/', chat, name='chat'),
+    path('register/', register, name='register'),
+    path('login/', CustomLoginView.as_view(template_name='Fleet/Auth/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
+    path('user-panel/', user_panel, name='user_panel'),
+]
