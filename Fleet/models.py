17a8a39385df62a46407ed576c3d0de1cb5f90e3
student-wroclaw <kingakusa@gmail.com>
2025-05-18 14:21:10 +0200

    7.1

--- Fleet/models.py
+++ Fleet/models.py
@@ -6,6 +6,12 @@ class Post(models.Model):
     content = models.TextField()
     create_at = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+    distance = models.FloatField(blank=True, null=True)
+    start_location = models.CharField(max_length=255, blank=True, null=True)
+    end_location = models.CharField(max_length=255, blank=True, null=True)
+    travel_time = models.DurationField(blank=True, null=True)
+    vehicle = models.CharField(max_length=100, blank=True, null=True)
+    image = models.ImageField(upload_to="post_images/", blank=True, null=True)
 
     def __str__(self):
         return self.title
