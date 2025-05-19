2437e3b54b31ea798c13d203381fa86cd52d9aeb
KingaKusa <kingakusa@gmail.com>
2025-05-19 20:31:10 +0200

    9.2

--- Fleet/models.py
+++ Fleet/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
-    create_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     distance = models.FloatField(blank=True, null=True)
     start_location = models.CharField(max_length=255, blank=True, null=True)
